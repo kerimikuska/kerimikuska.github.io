1c42561b31a8473a208dc22cbca5b4a810a80c88
kerimikuska <kmikuska@tribpub.com>
2015-10-21 20:59:08 -0500

    one-pager with sticky header

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// var navOffset = $('.main-nav').offset().top;
+
+
+//get the distance from the menu to the top of the page
+//use "offset" and store it as a variable
+//when you scroll down the nav starts at the top
+//when you scroll up, the nav goes back to its place
+
+// $(window).on('scroll', function () {
+// 	var scrollPos = $(this).scrollTop();
+	
+// 	if (scrollPos >= navOffset) {
+// 		$('.main-nav').addClass('scrolled');
+// 	} else {
+// 		$('.main-nav').removeClass('scrolled');
+// 	}
+// });
+
+// $('header').css('background-color', 'blue');
