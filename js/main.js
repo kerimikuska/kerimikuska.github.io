ab8f1c892eb2848a65eda0cff71781dab9e4de55
kerimikuska <kmikuska@tribpub.com>
2015-10-25 10:01:45 -0500

    update to caturdays
    canvas & instafeed

--- js/main.js
+++ js/main.js
@@ -17,3 +17,38 @@
 // });
 
 // $('header').css('background-color', 'blue');
+
+
+// ========== CANVAS BELOW ========== //
+
+// function draw(){
+// 	var canvas = document.getElementById('tutorial');
+// 	if (canvas.getContext){
+// 		var ctx = canvas.getContext('2d');
+//     }
+// }
+
+function draw() {
+      var canvas = document.getElementById("canvas");
+      if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+	
+	    ctx.beginPath();
+	    ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+	    ctx.moveTo(110,75);
+	    ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
+	    ctx.moveTo(65,65);
+	    ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+	    ctx.moveTo(95,65);
+	    ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+	    ctx.stroke();
+	  }
+    }
+
+// $('body').onloaddraw();
+
+$('.body').on('load', function () {
+	draw();
+});
+
+// ========== CANVAS ABOVE ========== //
