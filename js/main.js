d908eb13567f3e8f0ffaa9643e1e5d73b32ef618
kerimikuska <kmikuska@tribpub.com>
2015-10-25 16:42:53 -0500

    additional styles, form spree

--- js/main.js
+++ js/main.js
@@ -1,54 +1,21 @@
-// var navOffset = $('.main-nav').offset().top;
+// alert("keri's js file is working properly");
 
+//when someone hovers over "submit" button
+	//color changes to a darker shade
+	//shadow is added (both through .hover-button class)
 
-//get the distance from the menu to the top of the page
-//use "offset" and store it as a variable
-//when you scroll down the nav starts at the top
-//when you scroll up, the nav goes back to its place
-
-// $(window).on('scroll', function () {
-// 	var scrollPos = $(this).scrollTop();
-	
-// 	if (scrollPos >= navOffset) {
-// 		$('.main-nav').addClass('scrolled');
-// 	} else {
-// 		$('.main-nav').removeClass('scrolled');
-// 	}
-// });
-
-// $('header').css('background-color', 'blue');
-
-
-// ========== CANVAS BELOW ========== //
-
-// function draw(){
-// 	var canvas = document.getElementById('tutorial');
-// 	if (canvas.getContext){
-// 		var ctx = canvas.getContext('2d');
-//     }
-// }
-
-function draw() {
-      var canvas = document.getElementById("canvas");
-      if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-	
-	    ctx.beginPath();
-	    ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
-	    ctx.moveTo(110,75);
-	    ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
-	    ctx.moveTo(65,65);
-	    ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
-	    ctx.moveTo(95,65);
-	    ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
-	    ctx.stroke();
-	  }
-    }
-
-// $('body').onloaddraw();
-
-$('.body').on('load', function () {
-	draw();
+$('.submit').on('hover', function () {
+	$(this).addClass('hover-button');
 });
 
-// ========== CANVAS ABOVE ========== //
+
+
+
+//when someone clicks on "submit" button
+	//button moves down 1px to give effect of being pressed
+	//shadow moves to inset (maybe) (through .clicked-button class)
+
+	//and a small "thank you for submitting" message shows up in place of the form
+	//page does not reload
+
+
