694362e698166e70ca59d43e599b5dde0dbb8629
kerimikuska <kmikuska@tribpub.com>
2015-10-26 20:39:18 -0500

    woo! anchors work!

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 // alert("keri's js file is working properly");
 
-//when someone hovers over "submit" button
-	//color changes to a darker shade
-	//shadow is added (both through .hover-button class)
-
-// $('.submit').on('hover', function () {
-// 	$('.submit').addClass('hover-button');
-// });
-
-
-
-
 //when someone clicks on "submit" button
 	//button moves down 1px to give effect of being pressed
 	//shadow moves to inset (maybe) (through .clicked-button class)
@@ -18,21 +7,6 @@
 // $('.submit').on('click', function () {
 // 	$('.submit').addClass('clicked-button');
 // });
-
-
-	//and a small "thank you for submitting" message shows up in place of the form
-	//page does not reload
-
-
-
-//when someone clicks on any of the three Nav links (about, photos, contact).
-	//((FYI, this is currently setup w/ deprecated "name" anchors))
-	//jumps to respective id on page (#about, #photos, #contact)
-	//makes sure the entire section is visible (note: 80px hidden under header)
-
-
-//as a user scrolls down the page
-	//maybe some transition elements? not sure yet. 
 
 
 // Smooth scroll this mother!
@@ -49,7 +23,7 @@ $('nav a').on('click', function(e) {
   // use jQuery.animate() to animate the body's
   // scrollTop to the targetOffest
   $('body').animate({
-    scrollTop: targetOffset
+    scrollTop: targetOffset - 95
   }, 900);
 });
 
