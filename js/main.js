45cffabd4c73aefc6cfa977c94b08b9ced879cfb
kerimikuska <kmikuska@tribpub.com>
2015-10-26 20:34:23 -0500

    other stuff

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 	//color changes to a darker shade
 	//shadow is added (both through .hover-button class)
 
-$('.submit').on('hover', function () {
-	$('.submit').addClass('hover-button');
-});
+// $('.submit').on('hover', function () {
+// 	$('.submit').addClass('hover-button');
+// });
 
 
 
@@ -15,9 +15,9 @@ $('.submit').on('hover', function () {
 	//button moves down 1px to give effect of being pressed
 	//shadow moves to inset (maybe) (through .clicked-button class)
 
-$('.submit').on('click', function () {
-	$('.submit').addClass('clicked-button');
-});
+// $('.submit').on('click', function () {
+// 	$('.submit').addClass('clicked-button');
+// });
 
 
 	//and a small "thank you for submitting" message shows up in place of the form
@@ -33,3 +33,23 @@ $('.submit').on('click', function () {
 
 //as a user scrolls down the page
 	//maybe some transition elements? not sure yet. 
+
+
+// Smooth scroll this mother!
+
+// Do it when someone clicks a nav link
+$('nav a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 900);
+});
+
